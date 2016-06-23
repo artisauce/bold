18c7eeca93f20f21a60c585a8b63f17c2bc4c838
efilatovLegend <jenya.filatov@gmail.com>
2016-06-23 14:17:35 -0700

    Create battle.h

---
+++ battle.h
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <cstdlib>
+#include <iostream>
+#include "character.h"
+
+
+
+void battle_Player_Attacks(Group Attackers, Group Defenders);  // Case when the player attacks a group controlled by the computer
+
+void battle_Player_Defends(Group Attackers, Group Defenders);  // Case when the player is attacked  a group controlled by the computer
