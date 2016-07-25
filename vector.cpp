25b74afb759978f34c7fa9f9b403b689e60b9523
David <artisauce@gmail.com>
2016-07-25 12:21:07 -0700

    Vector experiment

--- vector.cpp
+++ vector.cpp
@@ -4,15 +4,28 @@ using namespace std;
  
 int main()
 {
-    vector<double> student_marks(20); // Manually specify
+    vector<double> student_marks;
  
-    for (vector<double>::size_type i = 0; i < 20; i++)
+    for (int i = 0; i < student_marks.size()+1; i++)
     {
         cout << "Enter marks for student #" << i+1 
              << ": " << flush;
-        cin >> student_marks[i];
+             double red = -1.0;
+        cin >> red;
+        if(red >= 0){
+        	student_marks.push_back(red);
+        	//cout << student_marks.capacity() << endl; Doubles every time after n = 4.
+        }
+        else{
+        	cout << endl;
+        	//cout << student_marks.capacity() << endl;
+        }
     }
-    // ... Do some stuff with the values
+    for (int i = 0; i < student_marks.size(); ++i)
+    {
+    	cout << "Marks for student #" << i+1 
+             << ": " << student_marks[i] << endl;
+    }
  
     return 0;
 }
