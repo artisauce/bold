2a754f16965b1dcea87e970ad6fa303597f2fc64
artisauce <artisauce@gmail.com>
2016-08-29 22:20:07 -0700

    Cleanup.

--- test.hpp
+++
@@ -1,6 +0,0 @@
-enum either {containsString = 0, containsInt = 1};
-class test {
-public:
-	test* testPoint;
-	either whichValue;
-};
--- test2.cpp
+++
@@ -1,4 +0,0 @@
-class test2 {
-public:
-	test1* testPoint1;
-};
