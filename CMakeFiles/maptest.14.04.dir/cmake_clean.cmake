871fd3ef10cd83759cc7d55f0770cc52280d72aa
artisauce <artisauce@gmail.com>
2016-10-27 23:55:54 -0700

    Further testing.

--- CMakeFiles/maptest.14.04.dir/cmake_clean.cmake
+++ CMakeFiles/maptest.14.04.dir/cmake_clean.cmake
@@ -1,15 +1,15 @@
-FILE(REMOVE_RECURSE
+file(REMOVE_RECURSE
   "CMakeFiles/maptest.14.04.dir/src/worldMap.cpp.o"
   "CMakeFiles/maptest.14.04.dir/src/tile.cpp.o"
-  "CMakeFiles/maptest.14.04.dir/src/battleField.cpp.o"
-  "CMakeFiles/maptest.14.04.dir/src/toolkit.cpp.o"
   "CMakeFiles/maptest.14.04.dir/src/map.cpp.o"
   "CMakeFiles/maptest.14.04.dir/src/maptest.cpp.o"
+  "CMakeFiles/maptest.14.04.dir/src/battleField.cpp.o"
+  "CMakeFiles/maptest.14.04.dir/src/toolkit.cpp.o"
   "build/maptest.14.04.pdb"
   "build/maptest.14.04"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/maptest.14.04.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/maptest.14.04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
