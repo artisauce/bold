cd94be3bdaa1c6a09751edbe9ab55a496ec34f60
David <artisauce@gmail.com>
2016-10-28 08:53:32 -0700

    Works on CSIL. Apparently the executable versions are both for the computers it was compiled on.

--- CMakeFiles/maptest.16.04.dir/cmake_clean.cmake
+++ CMakeFiles/maptest.16.04.dir/cmake_clean.cmake
@@ -1,15 +1,15 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/maptest.16.04.dir/src/worldMap.cpp.o"
-  "CMakeFiles/maptest.16.04.dir/src/tile.cpp.o"
+file(REMOVE_RECURSE
   "CMakeFiles/maptest.16.04.dir/src/battleField.cpp.o"
-  "CMakeFiles/maptest.16.04.dir/src/toolkit.cpp.o"
   "CMakeFiles/maptest.16.04.dir/src/map.cpp.o"
   "CMakeFiles/maptest.16.04.dir/src/maptest.cpp.o"
+  "CMakeFiles/maptest.16.04.dir/src/tile.cpp.o"
+  "CMakeFiles/maptest.16.04.dir/src/toolkit.cpp.o"
+  "CMakeFiles/maptest.16.04.dir/src/worldMap.cpp.o"
   "build/maptest.16.04.pdb"
   "build/maptest.16.04"
 )
 
 # Per-language clean rules from dependency scanning.
-FOREACH(lang CXX)
-  INCLUDE(CMakeFiles/maptest.16.04.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
+foreach(lang CXX)
+  include(CMakeFiles/maptest.16.04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
