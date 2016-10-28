9434760e0653fc2e55c2e5fb54296f2212b50c56
artisauce <artisauce@gmail.com>
2016-10-27 23:15:48 -0700

    Testing for 14.04 compatibility. Solution in works.

---
+++ CMakeFiles/maptest.14.04.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/gefer8/bold/src/battleField.cpp" "/home/gefer8/bold/CMakeFiles/maptest.14.04.dir/src/battleField.cpp.o"
+  "/home/gefer8/bold/src/map.cpp" "/home/gefer8/bold/CMakeFiles/maptest.14.04.dir/src/map.cpp.o"
+  "/home/gefer8/bold/src/maptest.cpp" "/home/gefer8/bold/CMakeFiles/maptest.14.04.dir/src/maptest.cpp.o"
+  "/home/gefer8/bold/src/tile.cpp" "/home/gefer8/bold/CMakeFiles/maptest.14.04.dir/src/tile.cpp.o"
+  "/home/gefer8/bold/src/toolkit.cpp" "/home/gefer8/bold/CMakeFiles/maptest.14.04.dir/src/toolkit.cpp.o"
+  "/home/gefer8/bold/src/worldMap.cpp" "/home/gefer8/bold/CMakeFiles/maptest.14.04.dir/src/worldMap.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "include"
+  "include/sdl"
+  "include/sdl_image"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
--- CMakeFiles/maptest.dir/DependInfo.cmake
+++ CMakeFiles/maptest.dir/DependInfo.cmake
@@ -1,9 +1,9 @@
 # The set of languages for which implicit dependencies are needed:
-set(CMAKE_DEPENDS_LANGUAGES
+SET(CMAKE_DEPENDS_LANGUAGES
   "CXX"
   )
 # The set of files for implicit dependencies of each language:
-set(CMAKE_DEPENDS_CHECK_CXX
+SET(CMAKE_DEPENDS_CHECK_CXX
   "/home/gefer8/bold/src/battleField.cpp" "/home/gefer8/bold/CMakeFiles/maptest.dir/src/battleField.cpp.o"
   "/home/gefer8/bold/src/map.cpp" "/home/gefer8/bold/CMakeFiles/maptest.dir/src/map.cpp.o"
   "/home/gefer8/bold/src/maptest.cpp" "/home/gefer8/bold/CMakeFiles/maptest.dir/src/maptest.cpp.o"
@@ -11,18 +11,18 @@ set(CMAKE_DEPENDS_CHECK_CXX
   "/home/gefer8/bold/src/toolkit.cpp" "/home/gefer8/bold/CMakeFiles/maptest.dir/src/toolkit.cpp.o"
   "/home/gefer8/bold/src/worldMap.cpp" "/home/gefer8/bold/CMakeFiles/maptest.dir/src/worldMap.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+SET(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
 
 # The include file search paths:
-set(CMAKE_CXX_TARGET_INCLUDE_PATH
+SET(CMAKE_C_TARGET_INCLUDE_PATH
   "include"
   "include/sdl"
   "include/sdl_image"
   )
-
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
-  )
-
-# Fortran module output directory.
-set(CMAKE_Fortran_TARGET_MODULE_DIR "")
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
